f90ba2eb3fe497ce5860e628fd7838e8c0757323
Alexian Moins <alexian.moins@outlook.com>
2023-06-24 18:17:08 +0200

    env

--- src/services/Strapi.ts
+++ src/services/Strapi.ts
@@ -1,12 +1,13 @@
 import { GraphQLClient } from 'graphql-request';
 import { getConfiguration } from '../helpers/config';
 import {getSdk} from "../services/GraphQL";
+import * as process from "process";
 
 const {} = getConfiguration();
 
 export const getStrapiClient = () =>
 {
     const client = new GraphQLClient(
-        new URL("/graphql", 'http://127.0.0.1:1337').toString());
+        new URL("/graphql", process.env.STRAPI_CMS_BASE_URL).toString());
     return getSdk(client);
 };
